aa547946b2c8bdf9e9dbe51401a1b7e127bcb61c
serega-patyukov <serega-patyukov@mail.ru>
2022-07-05 09:58:46 +0300

    Начало

---
+++ src/main/java/ru/patyukov/backpack_tourist/BackpackTouristApplication.java
@@ -0,0 +1,13 @@
+package ru.patyukov.backpack_tourist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackpackTouristApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackpackTouristApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/patyukov/backpack_tourist/BackpackTouristApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.patyukov.backpack_tourist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackpackTouristApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
