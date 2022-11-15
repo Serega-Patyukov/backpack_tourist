51f6d430ee85c6070795992a723adc495cdf8f27
serega-patyukov <serega-patyukov@mail.ru>
2022-11-15 21:30:31 +0300

    Страницу создания похода перенес на страницу туриста

--- src/main/java/ru/patyukov/backpack_tourist/web/controller/UserController.java
+++ src/main/java/ru/patyukov/backpack_tourist/web/controller/UserController.java
@@ -3,9 +3,13 @@ package ru.patyukov.backpack_tourist.web.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import ru.patyukov.backpack_tourist.facade.Facade;
 import ru.patyukov.backpack_tourist.security.SecurityContext;
+import ru.patyukov.backpack_tourist.web.request.HikeRequest;
+
+import javax.validation.Valid;
 
 @Controller
 @AllArgsConstructor
@@ -22,9 +26,12 @@ public class UserController {
 
     @ModelAttribute
     public void addModel(@RequestParam(required = false, defaultValue = "") String msg,
+                         @RequestParam(required = false, defaultValue = "") String addHike,
                          Model model) {
         model.addAttribute("msg", msg);
+        model.addAttribute("addHike", addHike);
         model.addAttribute("userResponse", facade.getUserResponse());
+        model.addAttribute("hikeRequest", new HikeRequest());
     }
 
     @GetMapping("/exit")
@@ -47,5 +54,16 @@ public class UserController {
     public String deleteUser(String login) {
         facade.deleteUser(login);
         return "redirect:/";
+    }
+
+    @PostMapping("/addHike")
+    public String saveHike(@Valid HikeRequest hikeRequest, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            model.addAttribute("msg", "Введите учетные данные корректно");
+            model.addAttribute("addHike", "add");
+            return "user";
+        }
+        facade.saveHike(hikeRequest);
+        return "redirect:/user";
     }
 }
--- src/main/java/ru/patyukov/backpack_tourist/web/request/HikeRequest.java
+++ src/main/java/ru/patyukov/backpack_tourist/web/request/HikeRequest.java
@@ -7,7 +7,7 @@ import javax.validation.constraints.Size;
 @Data
 public class HikeRequest {
     private long id;
-    @Size(min = 3, message = "Введите корректно имя. Минимум 3 символа.")
+    @Size(min = 3, message = "Минимум 3 символа.")
     private String name;
     private String dateStart;
     private String dateFinish;
